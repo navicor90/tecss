53327ec8183e42b12a37f9d8570c0708a7691d34
ivan <miltonivanterreno@gmail.com>
2017-09-09 11:37:45 -0300

    fixes

--- tecss2/settings.py
+++ tecss2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import dj_database_url.config()
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,14 +75,13 @@ WSGI_APPLICATION = 'tecss2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
