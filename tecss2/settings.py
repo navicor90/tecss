ec8d5efeb4cf35f12e924a84514f25028fbede44
navicor90 <miltonivanterreno@gmail.com>
2020-04-27 13:48:21 -0300

    mysql support

--- tecss2/settings.py
+++ tecss2/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'tecss2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tecss',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+db_url = os.getenv('CLEARDB_DATABASE_URL')
+if db_url:
+    DATABASES['default'] = dj_database_url.config(default=db_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
